66e19e80132c96fa30ec02fab17f9727759a91d6
Jay Khant <jay.khant.79@gmail.com>
2021-12-13 14:50:52 +0530

    Deploy with failed tests

--- tests/e2e/specs/about.js
+++ tests/e2e/specs/about.js
@@ -1,6 +1,6 @@
 describe("About", () => {
     it("Visits the about page", () => {
         cy.visit("/about")
-        cy.contains("h1", "This is an about page")
+        cy.contains("h1", "This is an about page1")
     })
 })
